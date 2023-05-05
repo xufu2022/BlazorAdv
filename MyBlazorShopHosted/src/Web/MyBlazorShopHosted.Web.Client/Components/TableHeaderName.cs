10f21d071d7bec86736d952f6d78e102159d38ba
Fu Xu <xufu2022@gmail.comm>
2023-05-05 15:30:45 +0100

    init

---
+++ MyBlazorShopHosted/src/Web/MyBlazorShopHosted.Web.Client/Components/TableHeaderName.cs
@@ -0,0 +1,11 @@
+﻿namespace MyBlazorShopHosted.Web.Client.Components
+{
+    public class TableHeaderName
+    {
+        public TableHeaderName(string name)
+        {
+            Name = name;
+        }
+        public string Name { get;}
+    }
+}
---
+++ MyBlazorShopHosted/src/Web/MyBlazorShopHosted.Web.Client/wwwroot/js/JSInterlop.js
@@ -0,0 +1,8 @@
+﻿function GetLocalOffsetMinutes() {
+    var date = new Date();
+
+    return date.getTimezoneOffset();
+}
+function AddToCart(productName) {
+    alert(productName + " has been added to the cart");
+}
